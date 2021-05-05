0ba9d0c7af995fb655f868ea237a8331fbfd7cf8
hans0 <kiessler.hans@gmail.com>
2021-05-05 16:28:16 -0400

    Initial commit, starting server

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it
+const server = require('./api/server.js');
+
+const portNumber = 5000;
+
+server.listen(portNumber, () => {
+  console.log(`\n* Server Running on http://localhost:${portNumber} *\n`)
+})
