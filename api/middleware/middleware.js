2c8afd9428612ffad4202a896a4526496ddd37cd
hans0 <kiessler.hans@gmail.com>
2021-05-05 19:09:52 -0400

    Able to update user

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -27,39 +27,54 @@ async function validateUserId(req, res, next) {
       next();
     }
   } catch (error) {
-    next(error);
+    res.status(500).json({
+      message: 'Problem finding user'
+    })
   }
 }
 
-async function validateUser (req, res, next) {
+function validateUser (req, res, next) {
   // validateUser validates the body on a request to create or update a user
   // if the request body lacks the required name field, respond with status 400 and { message: "missing required name field" }
   // DO YOUR MAGIC
-  if (!req.body.name || req.body.name.trim().length === 0){
-    res.status(400).json({ 
+  const { name } = req.body;
+  if (!name || name.trim().length === 0){
+    res.status(400).json({
       message: "missing required name field" 
     });
-    return;
+  } else {
+    req.name = name.trim();
+    next();
   }
-  Users.insert(req.body)
-    .then(user => {
-      res.status(200).json(user);
-      next(user);
-    })
-    .catch(error => {
-      console.log(error);
-      next(error);
-    })
+
+  // if (!req.body.name || req.body.name.trim().length === 0){
+  //   res.status(400).json({ 
+  //     message: "missing required name field" 
+  //   });
+  //   return;
+  // }
+  // Users.insert(req.body)
+  //   .then(user => {
+  //     res.status(200).json(user);
+  //     next(user);
+  //   })
+  //   .catch(error => {
+  //     console.log(error);
+  //     next(error);
+  //   })
 }
 
 function validatePost(req, res, next) {
   // validatePost validates the body on a request to create a new post
   // if the request body lacks the required text field, respond with status 400 and { message: "missing required text field" }
-  if (!req.body.text){
+  const { text } = req.body;
+  if (!text || !text.trim()){
     res.status(400).json({ 
       message: "missing required text field" 
-    });
-    return;
+    }); 
+  } else {
+    req.text = text.trim();
+    next();
   }
   // DO YOUR MAGIC
   // Post
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -15,7 +15,7 @@ const {
 const router = express.Router();
 
 
-router.get('/',  (req, res, next) => {
+router.get('/', (req, res, next) => {
   // RETURN AN ARRAY WITH ALL THE USERS
   Users.get(req.query)
     .then(users => {
@@ -25,41 +25,63 @@ router.get('/',  (req, res, next) => {
 });
 
 
-
-router.get('/:id', logger, validateUserId, (req, res) => {
+router.get('/:id',  validateUserId, (req, res) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
   res.json(req.user);
 });
 
-router.post('/', logger, validateUser, (req, res) => {
+router.post('/', validateUser, (req, res, next) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
-  res.json(req.user);
+  Users.insert(req.body)
+    .then(newUser => {
+      res.status(201).json(newUser);
+    })
+    .catch(next);
+    // equivalent to this
+    // .catch(error => next(error) )
+
 });
 
-router.put('/:id', logger, validateUserId, validateUser, (req, res) => {
+router.put('/:id', validateUserId, validateUser, (req, res, next) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
-  res.json(req.user);
+  Users.update(req.params.id, req.body)
+    .then(rowsChanged => { //eslint-disable-line
+      return Users.getById(req.params.id);
+    })
+    .then(upatedUser => {
+      res.status(200).json(upatedUser)
+    })
+    .catch(next);
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateUserId, (req, res) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
+  console.log(req.user);
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
 });
+
+router.use((err, req, res, next) => { //  eslint-disable-line
+  res.status(err.status || 500).json({
+    customMessage: 'something tragic insides posts router happened',
+    message: err.message,
+    stack: err.stack,
+  })
+})
 
 // do not forget to export the router
 module.exports = router;
